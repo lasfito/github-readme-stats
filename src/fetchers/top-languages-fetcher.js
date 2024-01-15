19826a2828b634f7c6b3acf7ea2dc606de5f0ded
Adrian <79036345+lasfito@users.noreply.github.com>
2024-01-15 14:41:25 +0900

    Update top-languages-fetcher.js

--- src/fetchers/top-languages-fetcher.js
+++ src/fetchers/top-languages-fetcher.js
@@ -1,33 +1,20 @@
 // @ts-check
+import * as dotenv from "dotenv";
 import { retryer } from "../common/retryer.js";
-import {
-  CustomError,
-  logger,
-  MissingParamError,
-  request,
-  wrapTextMultiline,
-} from "../common/utils.js";
-
+import { logger, MissingParamError, request } from "../common/utils.js";
+dotenv.config();
 /**
- * @typedef {import("axios").AxiosRequestHeaders} AxiosRequestHeaders Axios request headers.
- * @typedef {import("axios").AxiosResponse} AxiosResponse Axios response.
- */
-
-/**
- * Top languages fetcher object.
- *
- * @param {AxiosRequestHeaders} variables Fetcher variables.
- * @param {string} token GitHub token.
- * @returns {Promise<AxiosResponse>} Languages fetcher response.
+ * @param {import('Axios').AxiosRequestHeaders} variables
+ * @param {string} token
  */
 const fetcher = (variables, token) => {
   return request(
     {
       query: `
-      query userInfo($login: String!) {
+      query userInfo($login: String!, $after: String) {
         user(login: $login) {
           # fetch only owner repos & not forks
-          repositories(ownerAffiliations: OWNER, isFork: false, first: 500) {
+          repositories(ownerAffiliations: OWNER, isFork: false, first: 100, after: $after) {
             nodes {
               name
               languages(first: 10, orderBy: {field: SIZE, direction: DESC}) {
@@ -40,6 +27,10 @@ const fetcher = (variables, token) => {
                 }
               }
             }
+            pageInfo {
+              hasNextPage
+              endCursor
+            }
           }
         }
       }
@@ -51,53 +42,31 @@ const fetcher = (variables, token) => {
     },
   );
 };
-
 /**
- * @typedef {import("./types").TopLangData} TopLangData Top languages data.
+ * @param {string} username
+ * @param {string[]} exclude_repo
+ * @returns {Promise<import("./types").TopLangData>}
  */
+async function fetchTopLanguages(username, exclude_repo = []) {
+  if (!username) throw new MissingParamError(["username"]);
 
-/**
- * Fetch top languages for a given username.
- *
- * @param {string} username GitHub username.
- * @param {string[]} exclude_repo List of repositories to exclude.
- * @param {number} size_weight Weightage to be given to size.
- * @param {number} count_weight Weightage to be given to count.
- * @returns {Promise<TopLangData>} Top languages data.
- */
-const fetchTopLanguages = async (
-  username,
-  exclude_repo = [],
-  size_weight = 1,
-  count_weight = 0,
-) => {
-  if (!username) {
-    throw new MissingParamError(["username"]);
+  let repoNodes = [];
+  let hasNextPage = true;
+  let endCursor = null;
+  while (hasNextPage) {
+    const variables = { login: username, first: 100, after: endCursor };
+    const res = await retryer(fetcher, variables);
+
+    if (res.data.errors) {
+      logger.error(res.data.errors);
+      throw Error(res.data.errors[0].message || "Could not fetch user");
+    }
+
+    repoNodes.push(...res.data.data.user.repositories.nodes);
+    hasNextPage = res.data.data.user.repositories.pageInfo.hasNextPage;
+    endCursor = res.data.data.user.repositories.pageInfo.endCursor;
   }
 
-  const res = await retryer(fetcher, { login: username });
-
-  if (res.data.errors) {
-    logger.error(res.data.errors);
-    if (res.data.errors[0].type === "NOT_FOUND") {
-      throw new CustomError(
-        res.data.errors[0].message || "Could not fetch user.",
-        CustomError.USER_NOT_FOUND,
-      );
-    }
-    if (res.data.errors[0].message) {
-      throw new CustomError(
-        wrapTextMultiline(res.data.errors[0].message, 90, 1)[0],
-        res.statusText,
-      );
-    }
-    throw new CustomError(
-      "Something went wrong while trying to retrieve the language data using the GraphQL API.",
-      CustomError.GRAPHQL_ERROR,
-    );
-  }
-
-  let repoNodes = res.data.data.user.repositories.nodes;
   let repoToHide = {};
 
   // populate repoToHide map for quick lookup
@@ -107,14 +76,10 @@ const fetchTopLanguages = async (
       repoToHide[repoName] = true;
     });
   }
-
   // filter out repositories to be hidden
   repoNodes = repoNodes
     .sort((a, b) => b.size - a.size)
     .filter((name) => !repoToHide[name.name]);
-
-  let repoCount = 0;
-
   repoNodes = repoNodes
     .filter((node) => node.languages.edges.length > 0)
     // flatten the list of language nodes
@@ -122,17 +87,11 @@ const fetchTopLanguages = async (
     .reduce((acc, prev) => {
       // get the size of the language (bytes)
       let langSize = prev.size;
-
       // if we already have the language in the accumulator
       // & the current language name is same as previous name
-      // add the size to the language size and increase repoCount.
+      // add the size to the language size.
       if (acc[prev.node.name] && prev.node.name === acc[prev.node.name].name) {
         langSize = prev.size + acc[prev.node.name].size;
-        repoCount += 1;
-      } else {
-        // reset repoCount to 1
-        // language must exist in at least one repo to be detected
-        repoCount = 1;
       }
       return {
         ...acc,
@@ -140,27 +99,17 @@ const fetchTopLanguages = async (
           name: prev.node.name,
           color: prev.node.color,
           size: langSize,
-          count: repoCount,
         },
       };
     }, {});
 
-  Object.keys(repoNodes).forEach((name) => {
-    // comparison index calculation
-    repoNodes[name].size =
-      Math.pow(repoNodes[name].size, size_weight) *
-      Math.pow(repoNodes[name].count, count_weight);
-  });
-
-  const topLangs = Object.keys(repoNodes)
+  return Object.keys(repoNodes)
     .sort((a, b) => repoNodes[b].size - repoNodes[a].size)
     .reduce((result, key) => {
       result[key] = repoNodes[key];
       return result;
     }, {});
-
-  return topLangs;
-};
+}
 
 export { fetchTopLanguages };
 export default fetchTopLanguages;
